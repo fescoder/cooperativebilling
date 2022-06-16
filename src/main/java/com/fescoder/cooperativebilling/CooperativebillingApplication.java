c90d2674cc56346985962b8f0efaf92cc75dede8
fescoder <fescoder@gmail.com>
2022-06-16 13:52:46 -0300

    Packages created

---
+++ src/main/java/com/fescoder/cooperativebilling/CooperativebillingApplication.java
@@ -0,0 +1,13 @@
+package com.fescoder.cooperativebilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperativebillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooperativebillingApplication.class, args);
+	}
+
+}
